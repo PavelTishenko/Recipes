831c5c2a18966b9524dcc7cd1c5698484c92aab2
PavelTishenko <32521236+PavelTishenko@users.noreply.github.com>
2019-09-24 14:35:01 +0300

    throw data up

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { RecipeService } from './../../../shared/recipe.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipes.model';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
+  providers:[RecipeService]
 })
 export class RecipeItemComponent implements OnInit {
  @Input() recipe: Recipe;
+ @Output() recipeSelected = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+  this.recipeSelected.emit();   
   }
 
 }
